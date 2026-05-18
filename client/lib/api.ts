3ed9c790e95525e8538e5bb571c8dd253ebe735f
aigerim23 <39916772+aigerim23@users.noreply.github.com>
2026-05-18 23:55:04 +0500

    Update api.ts

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    headers: {
+    'ngrok-skip-browser-warning': 'true' // Этот заголовок отключит проверку ngrok
+  }
 })
 
 api.interceptors.request.use((config) => {
